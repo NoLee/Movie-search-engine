3d7bc4a1537627af0bbe181f1caad76e6fe8307a
NoLee <manolios93@hotmail.com>
2018-12-24 17:49:37 +0200

    Implement AJAX request to movieDB API

--- requests.ts
+++ requests.ts
@@ -1,22 +1,50 @@
-//handle the XLMhttpreuqest (jquery or js)
+import {changeMovieTemplate} from './UI'
+require('handlebars');
 
-function requestMovieByTitle(title:string, page:number = 1) {
-    let urlString:string = "https://api.themoviedb.org/3/search/movie?api_key=4e8e150788febd77a9f470586acbe5f3&language=en-US&query="+title+"&page="+page+"&include_adult=false";
+/**
+ * Request movies from the server using "title" as a search
+ * @param title movie title (partial)
+ * @param page page number to load (defaults to 1 if omitted)
+ */
+export function requestMovieByTitle(title:string, page:number = 1, method?:string) {
+    let urlString:string = "https://api.themoviedb.org/3/search/movie?api_key=4e8e150788febd77a9f470586acbe5f3&language=en-US&query="+title+"&page="+page;
+    ajaxRequest(urlString,method);
+}
 
+/**
+ * Request movies from the server using a specified filter
+ * @param filter movie filter like "popular", "latest", "top_rated", etc
+ * @param page page number to load (defaults to 1 if omitted)
+ */
+export function requestMovieByFilter(filter:string, page:number = 1, method?:string) {
+    let urlString:string = "https://api.themoviedb.org/3/movie/"+filter+"?api_key=4e8e150788febd77a9f470586acbe5f3&language=en-US&page="+page;
+    ajaxRequest(urlString,method);
+};
+
+/**
+ * Send an async ajax request to the url specified
+ */
+function ajaxRequest(url:string,method?:string) {
     let req= $.ajax({
-        url: urlString
+        url: url
     })
-
     req.done(function( data ) {
-       showResults(data);
+        addStarRating(data);
+        changeMovieTemplate(data,method);        
     });
 }
 
+/**
+ * Adds an attribute ("star_rating" array) to the JSON object returned by themovieDB API 
+ */
+function addStarRating(data:any) {
+    data.results.forEach( (result:any) => {
+        let avg = result.vote_average;
+        result["star_rating"] = [];
 
-function showResults(data:any) {
-    console.log(data);
+        for (let i=0; i< 10; i++) {
+            if (i <= avg - 1) result["star_rating"][i] = "selected"
+            else result["star_rating"][i] = null
+        }
+    });
 }
-
-// IMAGE LINK: https://image.tmdb.org/t/p/w500/ + poster path
-
-requestMovieByTitle("captain")
