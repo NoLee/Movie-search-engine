285983457834d5b9b91f715a9cedd3eab5463f01
NoLee <manolios93@hotmail.com>
2018-12-27 15:27:32 +0200

    Added onclick event for movies

--- app.ts
+++ app.ts
@@ -4,52 +4,59 @@ import * as requests from "./requests";
 var timeoutID:any;
 var page:number = 1;
 var query:string = ":popular";
+var noScroll = false;
+
+// Onload fetch default movies
+window.onload = () => selectRequest(query);
+
+// If reached the bottom of the page, fetch next page and APPEND results
+$(window).scroll(function() {
+    let winScroll = $(window).scrollTop();
+    let winHeight = $(window).height();
+    let docHeight = $(document).height();    
+    if ( !noScroll && winScroll != undefined && winHeight != undefined && docHeight != undefined) {
+        if(winScroll + winHeight > docHeight -100) { 
+            selectRequest(query,"append");
+        }
+    }
+});
 
 // Listen for keystroke events in the search field
-$("#movie-query").keyup( function () {
+$("#movie-query").keyup( function() {
     let inputElement = <HTMLInputElement> this;    
     query = inputElement.value;
     // Reset page counter for a new search
     page = 1;
-    makeRequest();
+
+    // Clear the timeout if it has already been set
+    clearTimeout(timeoutID);
+    // Make a new timeout
+    timeoutID = setTimeout(function() {
+        noScroll = false;
+        selectRequest(query)
+    }, 700);
 });
 
-// Setup an ajax request after a set amount of time (to limit API requests)
-function makeRequest(method?:string) {
-        // Clear the timeout if it has already been set
-        clearTimeout(timeoutID);
-        // Make a new timeout
-        timeoutID = setTimeout(function () {
-            selectRequest(method)
-        }, 700);
-}
 
 /**
  * Select the type of request (by movie name or by filter) and then send it with AJAX
- * @param method (optional) if it is set to "append" it does not replace the previous results
+ * @param contentMethodSetting (optional) if it is set to "append" it does not replace the previous results
  */
-function selectRequest(method?:string) {
-    if ( $.inArray(query, [':now_playing', ':popular', ':top_rated']) >= 0 ) { 
-        requests.requestMovieByFilter(query.slice(1),page,method); 
+function selectRequest(q:string, contentMethodSetting?:string) {
+    if ( q[0] === ":" ) { 
+        requests.requestMovieByFilter(q.slice(1),page,contentMethodSetting); 
     }
-    else requests.requestMovieByTitle(query,page,method);
-    //Incement page counter for next page (if requested)
+    else requests.requestMovieByTitle(q,page,contentMethodSetting);
+    //Incement page counter for next page (when requested)
     page++;
 }
 
-// Onload fetch default movies
-window.onload = () => makeRequest();
-
-
-// If reached the bottom of the page, fetch next page and APPEND results
-$(window).scroll(function () {
-    let winScroll = $(window).scrollTop();
-    let winHeight = $(window).height();
-    let docHeight = $(document).height();
-    
-    if ( winScroll != undefined && winHeight != undefined && docHeight != undefined) {
-        if(winScroll + winHeight > docHeight -100) { 
-            makeRequest("append");
-        }
-    }
-});
+export function addEventListenerMovies() {
+    $(".movie-container").click( function() {
+        console.log("click");
+        noScroll = true;
+        let id = this.getAttribute("movie-id");
+        query = ":"+id;        
+        selectRequest(query);
+    });
+}
