b3de68cb7382fefc59df006a1d8d9f27c7d83d69
NoLee <manolios93@hotmail.com>
2018-12-27 15:27:53 +0200

    Various fixes and refactors

--- requests.ts
+++ requests.ts
@@ -1,4 +1,4 @@
-import {changeMovieTemplate} from './UI'
+import {changeMovieTemplate, resetMovieTemplate, showLoading, hideLoading,showMovieNotFound} from './UI'
 require('handlebars');
 
 /**
@@ -6,9 +6,9 @@ require('handlebars');
  * @param title movie title (partial)
  * @param page page number to load (defaults to 1 if omitted)
  */
-export function requestMovieByTitle(title:string, page:number = 1, method?:string) {
+export function requestMovieByTitle(title:string, page:number = 1, contentMethodSetting?:string) {
     let urlString:string = "https://api.themoviedb.org/3/search/movie?api_key=4e8e150788febd77a9f470586acbe5f3&language=en-US&query="+title+"&page="+page;
-    ajaxRequest(urlString,method);
+    ajaxRequest(urlString,contentMethodSetting);
 }
 
 /**
@@ -16,35 +16,54 @@ export function requestMovieByTitle(title:string, page:number = 1, method?:strin
  * @param filter movie filter like "popular", "latest", "top_rated", etc
  * @param page page number to load (defaults to 1 if omitted)
  */
-export function requestMovieByFilter(filter:string, page:number = 1, method?:string) {
+export function requestMovieByFilter(filter:string, page:number = 1, contentMethodSetting?:string) {
     let urlString:string = "https://api.themoviedb.org/3/movie/"+filter+"?api_key=4e8e150788febd77a9f470586acbe5f3&language=en-US&page="+page;
-    ajaxRequest(urlString,method);
+    ajaxRequest(urlString,contentMethodSetting);
 };
 
 /**
  * Send an async ajax request to the url specified
  */
-function ajaxRequest(url:string,method?:string) {
+function ajaxRequest(url:string,contentMethodSetting?:string) {
     let req= $.ajax({
-        url: url
+        url: url,
+        beforeSend: function () { 
+            if (contentMethodSetting !== "append") {
+                resetMovieTemplate();
+                showLoading(); 
+            }
+        }
     })
-    req.done(function( data ) {
+    .done(function(data) {
         addStarRating(data);
-        changeMovieTemplate(data,method);        
-    });
+        console.log(data);
+        changeMovieTemplate(data,contentMethodSetting);    
+    })
+    .fail(function(textStatus) {
+        showMovieNotFound();
+    })
+    .always( () => hideLoading() )
 }
 
 /**
- * Adds an attribute ("star_rating" array) to the JSON object returned by themovieDB API 
+ * Adds an attribute ("star_rating" array according to the average vote rating) to the JSON object returned by themovieDB API 
  */
 function addStarRating(data:any) {
-    data.results.forEach( (result:any) => {
+    if (data.results) {
+        data.results.forEach( (result:any) => {
+            addArray(result);
+        });
+    }
+    else {
+        addArray(data);
+    }
+    
+    function addArray(result:any) {
         let avg = result.vote_average;
-        result["star_rating"] = [];
-
-        for (let i=0; i< 10; i++) {
-            if (i <= avg - 1) result["star_rating"][i] = "selected"
-            else result["star_rating"][i] = null
-        }
-    });
+            result["star_rating"] = [];
+            for (let i=0; i< 10; i++) {
+                if (i <= avg - 1) result["star_rating"][i] = "selected"
+                else result["star_rating"][i] = null
+            }
+    }
 }
