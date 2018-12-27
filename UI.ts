50d33e3d9a4346ecfa63502f493d80e2107e271d
NoLee <manolios93@hotmail.com>
2018-12-27 15:26:06 +0200

    Added show() and hide() functions for various elements

--- UI.ts
+++ UI.ts
@@ -1,12 +1,44 @@
+import {addEventListenerMovies} from "./app"
+
 /**
  * Updates the HTML Handlebars template for the movie search
  * @param data The JSON object returned by themovieDB API
- * @param method (optional) If equals "append", then we append the data instead of updating
+ * @param contentMethodSetting (optional) If equals "append", then we append the data instead of updating
  */
-export function changeMovieTemplate(data:any, method?:string):void {    
+export function changeMovieTemplate(data:any, contentMethodSetting?:string):void {    
     let source   = $("#movie-template").html();
-    let template = Handlebars.compile(source);      
-    var theCompiledHtml = template(data);
-    if (method === "append") $('#handlebars-movie-container').append(theCompiledHtml);
-    else $('#handlebars-movie-container').html(theCompiledHtml);
+    let template = Handlebars.compile(source);  
+    if (data.total_results === 0) {
+        showMovieNotFound();
+    }   
+    else {
+        hideMovieNotFound();
+        var theCompiledHtml = template(data);
+        if (contentMethodSetting === "append") $('#handlebars-movie-container').append(theCompiledHtml);
+        else $('#handlebars-movie-container').html(theCompiledHtml);
+        addEventListenerMovies();
+    }
+} 
+
+// Show loading indication before fetching the results
+export function showLoading() {
+    $('.loading').removeClass('hidden');
+}
+
+// Hide loading indication after fetching the results
+export function hideLoading() {
+    $('.loading').addClass('hidden');
+}
+
+// Delete all the movies from screen 
+export function resetMovieTemplate() {
+    $('#handlebars-movie-container').html("");
+}
+
+export function showMovieNotFound() {
+    $('.no-movies').removeClass('hidden');
+}
+
+export function hideMovieNotFound() {
+    $('.no-movies').addClass('hidden');
 }
